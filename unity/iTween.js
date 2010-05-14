d52b73569dbaede7f86f6b67bd05f439507fc952
bobb@pixelplacement.com <bobb@pixelplacement.com@60b5d97b-30ca-493c-206d-367d9a6e2b36>
2010-05-14 18:56:31 +0000

    Added correct copyright header with license information
    git-svn-id: http://itween.googlecode.com/svn/trunk@3 60b5d97b-30ca-493c-206d-367d9a6e2b36

--- unity/iTween.js
+++ unity/iTween.js
@@ -1,3 +1,49 @@
+/**
+ * iTween
+ * Animation/tween framework for Unity based off the mechanisims and solutions established in the Flash tween world
+ *
+ * @author Bob Berkebile, Patrick Corkum
+ * @version 2.0.0
+ */
+
+/*
+Licensed under the MIT License
+
+Copyright (c) 2009-2010 Bob Berkebile, Patrick Corkum
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+http://itween.pixelplacement.com
+http://code.google.com/p/itween/
+*/
+
+/*
+TERMS OF USE - EASING EQUATIONS
+Open source under the BSD License.
+Copyright (c)2001 Robert Penner
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+Neither the name of the author nor the names of contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 //########//# VARS #//########static var tweens : Array = [];static var defaults : Hashtable = {"time":1,"delay":0,"transition":"easeInOutCubic","isLocal":false};private var transitions : Hashtable = {"easeInQuad":easeInQuad, "easeOutQuad":easeOutQuad,"easeInOutQuad":easeInOutQuad, "easeInCubic":easeInCubic, "easeOutCubic":easeOutCubic, "easeInOutCubic":easeInOutCubic, "easeInQuart":easeInQuart, "easeOutQuart":easeOutQuart, "easeInOutQuart":easeInOutQuart, "easeInQuint":easeInQuint, "easeOutQuint":easeOutQuint, "easeInOutQuint":easeInOutQuint, "easeInSine":easeInSine, "easeOutSine":easeOutSine, "easeInOutSine":easeInOutSine, "easeInExpo":easeInExpo, "easeOutExpo":easeOutExpo, "easeInOutExpo":easeInOutExpo, "easeInCirc":easeInCirc, "easeOutCirc":easeOutCirc, "easeInOutCirc":easeInOutCirc, "linear":linear, "spring":spring, "bounce":bounce, "easeInBack":easeInBack, "easeOutBack":easeOutBack, "easeInOutBack":easeInOutBack}; var id : String;var type : String;var method : String;var running : boolean;
 var time : float = defaults["time"];var delay : float = defaults["delay"];private var transition = linear;private var args : Hashtable;private var kinematicToggle : boolean;private var isLocal : boolean = defaults["isLocal"];private var runningTime : float = 0;private var percentage : float = 0;private var caluclatedFloat : float;private var prevVector3 : Vector3;private var calculatedVector3 : Vector3;private var startVector3 : Vector3;private var endVector3 : Vector3;private var startColor : Color;private var endColor : Color;//#############//# REGISTERS #//#############
 //movement:static function moveTo(target : GameObject, args : Hashtable) : void{	args["target"]=target;	args["id"]=generateID();	args["type"]="move";	if(!args.Contains("method")){		args["method"]="to";	}	init(target,args);}
