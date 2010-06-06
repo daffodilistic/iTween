2ba76edee0865ef43d13959c5569b8916115fd9c
pixelplacement <pixelplacement@60b5d97b-30ca-493c-206d-367d9a6e2b36>
2010-06-06 01:55:17 +0000

    Clean up of the C# version to get ready for 2.0
    git-svn-id: http://itween.googlecode.com/svn/trunk@126 60b5d97b-30ca-493c-206d-367d9a6e2b36

--- com/pixelplacement/itween/iTween.cs
+++ com/pixelplacement/itween/iTween.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class iTween : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
