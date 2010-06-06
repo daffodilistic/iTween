73fa9262029fd13a65a5401926264c8cdaa25a89
pixelplacement <pixelplacement@60b5d97b-30ca-493c-206d-367d9a6e2b36>
2010-06-06 02:48:18 +0000

    Added Hash method for "easier" hashtable creation as one of the three methods the C# version of iTween will leverage
    git-svn-id: http://itween.googlecode.com/svn/trunk@127 60b5d97b-30ca-493c-206d-367d9a6e2b36

--- com/pixelplacement/itween/iTween.cs
+++ com/pixelplacement/itween/iTween.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 using System.Collections;
+using System.Reflection;
 
 public class iTween : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	//C# version will have 3 methods for setting up tweens: typical hashtable entry (to keep the JS interface simple), typical hashtable with the use of the Hash method for "easier" entry, and finally the same method Unity uses for GUILayout (i.e. iTween.y(20))
 	
-	}
+	//##################################
+	//# INTERNAL HASH CREATION UTILITY #
+	//##################################
 	
-	// Update is called once per frame
-	void Update () {
-	
+	public static Hashtable Hash(params object[] args){
+		Hashtable hashTable = new Hashtable(args.Length/2);
+		if (args.Length %2 != 0) {
+			Debug.LogError("Tween Error: Hash requires an even number of arguments!"); 
+		}else{
+			int i = 0;
+			while(i < args.Length - 1) {
+				hashTable.Add(args[i], args[i+1]);
+				i += 2;
+			}
+		}
+		return hashTable;
 	}
 }
