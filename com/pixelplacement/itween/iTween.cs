695c70d60cd3ae603a3134926f7df9a2bc41ffd0
pixelplacement <pixelplacement@60b5d97b-30ca-493c-206d-367d9a6e2b36>
2010-08-12 14:47:57 +0000

    Incremented version number to 2.0.0.02
    git-svn-id: http://itween.googlecode.com/svn/trunk@247 60b5d97b-30ca-493c-206d-367d9a6e2b36

--- com/pixelplacement/itween/iTween.cs
+++ com/pixelplacement/itween/iTween.cs
@@ -24,7 +24,7 @@ using UnityEngine;
 #endregion
 
 /// <summary>
-/// <para>Version: 2.0.01</para>	 
+/// <para>Version: 2.0.02</para>	 
 /// <para>Author: Bob Berkebile (http://pixelplacement.com)</para>
 /// <para>Support: http://itween.pixelplacement.com</para>
 /// </summary>
