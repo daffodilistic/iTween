03649a594e2f3875bb006ab3067efdc2654edc2a
pixelplacement <pixelplacement@60b5d97b-30ca-493c-206d-367d9a6e2b36>
2010-08-13 07:28:24 +0000

    Removed int variable requirement for CameraFadeDestroy
    git-svn-id: http://itween.googlecode.com/svn/trunk@249 60b5d97b-30ca-493c-206d-367d9a6e2b36

--- com/pixelplacement/itween/iTween.cs
+++ com/pixelplacement/itween/iTween.cs
@@ -24,7 +24,7 @@ using UnityEngine;
 #endregion
 
 /// <summary>
-/// <para>Version: 2.0.03</para>	 
+/// <para>Version: 2.0.04</para>	 
 /// <para>Author: Bob Berkebile (http://pixelplacement.com)</para>
 /// <para>Support: http://itween.pixelplacement.com</para>
 /// </summary>
@@ -5018,7 +5018,7 @@ public class iTween : MonoBehaviour{
 	/// <summary>
 	/// Removes and destroyes a camera fade.
 	/// </summary>
-	public static void CameraFadeDestroy(int depth){
+	public static void CameraFadeDestroy(){
 		if(cameraFade){
 			Destroy(cameraFade);
 		}
